1213c427fc73ca617ac4f31e434a046a2bd7aac8
Zazalt <liviu+zazalt+github.com@sindla.com>
2017-01-17 00:23:52 +0200

    The cake is a lie.

--- Ubuntu+PHP5.6+PostgreSQL+Composer+PHPUnit/php.sh
+++ Ubuntu+PHP5.6+PostgreSQL+Composer+PHPUnit/php.sh
@@ -6,6 +6,12 @@ sed -i \
 	-e 's/^short_open_tag = Off/short_open_tag = On/g' \
 	-e 's/;date.timezone =/date.timezone = UTC/g' \
 	-e 's/expose_php = On/expose_php = Off/g' \
-	/etc/php.ini
+	/etc/php/5.6/cli/php.ini
 
-php -r 'echo date_default_timezone_get();'
+sed -i \
+	-e 's/^short_open_tag = Off/short_open_tag = On/g' \
+	-e 's/;date.timezone =/date.timezone = UTC/g' \
+	-e 's/expose_php = On/expose_php = Off/g' \
+	/etc/php/5.6/fpm/php.ini
+
+php -r "echo date_default_timezone_get();"
